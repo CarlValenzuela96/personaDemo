e2c6c75f48480455904643919f7f2623d4779948
Carl <c.valenzuela06@ufromail.cl>
2018-11-27 21:45:37 -0300

    prueba unitaria

--- src/test/java/com/carlos/personaDemo/PersonaDemoApplicationTests.java
+++ src/test/java/com/carlos/personaDemo/PersonaDemoApplicationTests.java
@@ -5,14 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonaDemoApplicationTests {
 
-	@Test
+	/**@Test
 	public void contextLoads() {
 
+	}*/
+
+	@Test
+	public void sumaTest(){
+		assertEquals(1,1);
 	}
 
 }
